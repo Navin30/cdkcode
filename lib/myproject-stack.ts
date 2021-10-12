f5bf4b544012b727a698beacf9a282a8c44c2bd1
Navin30 <navin.murugesan@nutechnologyinc.com>
2021-10-12 23:52:49 +0530

    myfirst

--- lib/myproject-stack.ts
+++ lib/myproject-stack.ts
@@ -1,9 +1,131 @@
 import * as cdk from '@aws-cdk/core';
-
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
+import * as rds from '@aws-cdk/aws-rds';
+import * as s3 from '@aws-cdk/aws-s3';
 export class MyprojectStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+  // 👇 create the VPC
+  const vpc=new ec2.Vpc(this,'VPC-tsc',{
+    cidr:'10.0.0.0/16',
+    maxAzs:3,
+    subnetConfiguration:[
+      {
+        subnetType:ec2.SubnetType.PUBLIC,
+        name:'VPC-Public',
+        cidrMask:24,
+      },
+      {
+        subnetType:ec2.SubnetType.PRIVATE_ISOLATED,
+        name:'VPC-Private',
+        cidrMask:24,
+      }
+    ], 
+    })
+      // 👇 create a security group for the EC2 instance
+    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+      vpc,
+      allowAllOutbound: true,
+    });
 
-    // The code that defines your stack goes here
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.ipv4(vpc.vpcCidrBlock),
+      ec2.Port.tcp(3306),
+      'rds'
+    );
+    
+    const webserverRole = new iam.Role(this, 'webserver-role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'),
+      ],
+    });
+   
+    
+    // 👇 create instance
+    const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      role: webserverRole,
+      securityGroup: webserverSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName:'key',
+    });
+    // 👇 create RDS instance
+    const dbInstance = new rds.DatabaseInstance(this, 'db-instance', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.ISOLATED,
+      },
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_13_1,
+      }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      credentials: rds.Credentials.fromGeneratedSecret('postgres'),
+      multiAz: false,
+      allocatedStorage: 100,
+      maxAllocatedStorage: 105,
+      allowMajorVersionUpgrade: false,
+      autoMinorVersionUpgrade: true,
+      
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+      databaseName: 'todosdb',
+      publiclyAccessible: false,
+    });
+
+    dbInstance.connections.allowFrom(ec2Instance, ec2.Port.tcp(3306));
+
+    new cdk.CfnOutput(this, 'dbEndpoint', {
+      value: dbInstance.instanceEndpoint.hostname,
+    });
+
+    new cdk.CfnOutput(this, 'secretName', {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+      value: dbInstance.secret?.secretName!,
+    });
+
+    // 👇 create s3
+    new s3.Bucket(this, 'nv-001', {
+      bucketName: 'nv-001',
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+      
+      });
+   
+    
   }
 }
+
+
